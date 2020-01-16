28d72b141ef27e4cf90f46a079fc74f9ccd85b48
natalie sinner <nataliesinner@hotmail.com>
2020-01-15 19:59:07 -0800

    prompt two

--- js/app.js
+++ js/app.js
@@ -8,8 +8,17 @@ var natalieAge = prompt('Is Natalie 27 years old?');
 if (natalieAge.toLocaleLowerCase() === 'yes') {
   alert('You are correct!');
 } else if (natalieAge.toLocaleLowerCase() === 'no') {
-  alert('You are wrong');
+  alert('You are incorrect');
 } else {
-  alert('Something wrong');
+  alert('Please add a numerical answer');
 }
 
+var hairColor = prompt('Is Natalies hair color blue?');
+
+if (hairColor.toLowerCase() === 'yes') {
+  alert('You are incorrect');
+} else if (hairColor.toLowerCase() === 'no') {
+  alert('That is correct, my hair color is brown');
+} else {
+  alert('Please answer with yes or no');
+}
