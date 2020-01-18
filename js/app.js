48219c3b5a787fe398160a9478fcbe22e02ed585
natalie sinner <nataliesinner@hotmail.com>
2020-01-18 14:12:26 -0800

    loops

--- js/app.js
+++ js/app.js
@@ -1,84 +1,85 @@
 'use strict';
 
-var userName = prompt('What is your name?');
-console.log(userName);
+// var userName = prompt('What is your name?');
+// console.log(userName);
 
-var greetUser = prompt('Hello,' + userName + '. Are your ready to play a game?');
+// var greetUser = prompt('Hello,' + userName + '. Are your ready to play a game?');
 
-if (greetUser.toLowerCase() === 'yes') {
-  alert('You chose wisely');
-} else if (greetUser.toLowerCase() === 'no') {
-  alert('You chose poorly');
-} else {
-  alert('You are going to play this game wether you like it or not');
-}
+// if (greetUser.toLowerCase() === 'yes') {
+//   alert('You chose wisely');
+// } else if (greetUser.toLowerCase() === 'no') {
+//   alert('You chose poorly');
+// } else {
+//   alert('You are going to play this game wether you like it or not');
+// }
 
-var natalieAge = prompt('Is Natalie 27 years old?');
-// console.log(natalieAge);
+// var natalieAge = prompt('Is Natalie 27 years old?');
+// // console.log(natalieAge);
 
-if (natalieAge.toLocaleLowerCase() === 'yes') {
-  alert('You are correct!');
-} else if (natalieAge.toLocaleLowerCase() === 'no') {
-  alert('You are incorrect');
-} else {
-  alert('Please add a numerical answer');
-}
+// if (natalieAge.toLocaleLowerCase() === 'yes') {
+//   alert('You are correct!');
+// } else if (natalieAge.toLocaleLowerCase() === 'no') {
+//   alert('You are incorrect');
+// } else {
+//   alert('Please add a numerical answer');
+// }
 
-var hairColor = prompt('Is Natalies hair color blue?');
-// console.log(hairColor);
+// var hairColor = prompt('Is Natalies hair color blue?');
+// // console.log(hairColor);
 
-if (hairColor.toLowerCase() === 'yes') {
-  alert('You are incorrect');
-} else if (hairColor.toLowerCase() === 'no') {
-  alert('That is correct, I have brown hair');
-} else {
-  alert('Please answer with yes or no');
-}
+// if (hairColor.toLowerCase() === 'yes') {
+//   alert('You are incorrect');
+// } else if (hairColor.toLowerCase() === 'no') {
+//   alert('That is correct, I have brown hair');
+// } else {
+//   alert('Please answer with yes or no');
+// }
 
-var likeWine = prompt('Does Natalie like wine?');
-// console.log(likeWine);
+// var likeWine = prompt('Does Natalie like wine?');
+// // console.log(likeWine);
 
-if (likeWine.toLowerCase() === 'yes') {
-  alert('That is correct, I like red and white wines');
-} else if (likeWine.toLowerCase() === 'no') {
-  alert('What are you talkin bout Willis, I LOVE wine!');
-} else {
-  alert('Please answer with yes or no');
-}
+// if (likeWine.toLowerCase() === 'yes') {
+//   alert('That is correct, I like red and white wines');
+// } else if (likeWine.toLowerCase() === 'no') {
+//   alert('What are you talkin bout Willis, I LOVE wine!');
+// } else {
+//   alert('Please answer with yes or no');
+// }
 
-var doesCrossFit = prompt('Does Natalie workout?');
-// console.log(doesCrossFit);
+// var doesCrossFit = prompt('Does Natalie workout?');
+// // console.log(doesCrossFit);
 
-if (doesCrossFit.toLowerCase() === 'yes') {
-  alert('Correct, Natalie enjoys CrossFit');
-} else if (doesCrossFit.toLowerCase() === 'no') {
-  alert('You are incorrect');
-} else {
-  alert('Please answer with yes or no');
-}
+// if (doesCrossFit.toLowerCase() === 'yes') {
+//   alert('Correct, Natalie enjoys CrossFit');
+// } else if (doesCrossFit.toLowerCase() === 'no') {
+//   alert('You are incorrect');
+// } else {
+//   alert('Please answer with yes or no');
+// }
 
-var vehicleType = prompt('Does Natalie drive a Mini Cooper?');
-// console.log(vehicleType);
+// // var vehicleType = prompt('Does Natalie drive a Mini Cooper?');
+// // console.log(vehicleType);
 
-if (vehicleType.toLowerCase() === 'yes') {
-  alert(userName + ' Correct! My Mini Cooper is named Sheldon');
-} else if (vehicleType.toLowerCase() === 'no') {
-  alert(userName + ' You have been tricked, the vehicle that I am driving to school is my mothers');
-} else {
-  alert(userName + ' Please answer with yes or no');
-}
+// // if (vehicleType.toLowerCase() === 'yes') {
+//   alert(userName + ' Correct! My Mini Cooper is named Sheldon');
+// } else if (vehicleType.toLowerCase() === 'no') {
+//   alert(userName + ' You have been tricked, the vehicle that I am driving to school is my mothers');
+// } else {
+//   alert(userName + ' Please answer with yes or no');
+// }
 
 // var endMessage = prompt('Thanks for playing ' + userName);
 // console.log(endMessage);
 
-var numericalAnswer = prompt('How many states has Natalie lived in?');
+// var numericalAnswer = prompt('How many states has Natalie lived in?');
 console.log(numericalAnswer);
 
-var numericalAnswer = [2];
+var tries = 0;
 
-while (numericalAnswer < 2) {
+while (tries < 2) {
+    var numericalAnswer = prompt('How many states has Natalie lived in?');
     console.log('answer less then 2');
-    numericalAnswer++;
+    tries++;
 }
 
 if (numericalAnswer === 2 ) {
